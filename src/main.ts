1de164232ca77a7f73748da87f9996983c9331fa
Michael_Scheer <macas.scheer@gmail.com>
2024-04-04 15:45:14 -0700

    remove test route add back fillter api logic

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,8 @@ import { config } from 'dotenv';
 
 const app = express();
 const port = process.env.PORT;
-app.get('/', (req, res) => {
-    res.send("<h1>HELLO FROM RAILWAY</h1>");
-});
-// app.get('/', filters);
+
+app.get('/', filters);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
